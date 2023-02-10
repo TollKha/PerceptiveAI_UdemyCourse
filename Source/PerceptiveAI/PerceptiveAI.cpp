5923df3c22db6906ee9e4deb401b573ec815d887
Anatolii Svyrydenko <metaluger@gmail.com>
2023-02-10 20:53:27 +0200

    Project based on chapters 1-4

---
+++ Source/PerceptiveAI/PerceptiveAI.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "PerceptiveAI.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, PerceptiveAI, "PerceptiveAI" );
---
+++ Source/PerceptiveAI/PerceptiveAIGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "PerceptiveAIGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PERCEPTIVEAI_API APerceptiveAIGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
